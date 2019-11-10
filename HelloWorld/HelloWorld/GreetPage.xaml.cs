9dd538c4e3c4beaeecb12b595b459aebcef8e538
Vatan Soni <vatansoni89@gmail.com>
2019-11-11 01:44:49 +0530

    Clean binding and detecting platform at code-behind.

--- HelloWorld/HelloWorld/GreetPage.xaml.cs
+++ HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -15,6 +15,18 @@ namespace HelloWorld
         public GreetPage()
         {
             InitializeComponent();
+
+            //To set padding considering different platforms.
+            switch (Device.RuntimePlatform)
+            {
+                case Device.Android:
+                    Padding = new Thickness(0, 20, 0, 0);
+                    break;
+
+                case Device.iOS:
+                    Padding = new Thickness(0, 30, 0, 0);
+                    break;
+            }
         }
     }
 }
