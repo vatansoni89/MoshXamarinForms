5869a790b7532ed19e02e02f93dd45e350b70bd5
Vatan Soni <vatansoni89@gmail.com>
2019-11-11 00:01:36 +0530

    Content Property

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -11,7 +11,7 @@ namespace HelloWorld
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new GreetPage();
         }
 
         protected override void OnStart()
---
+++ HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GreetPage : ContentPage
+	{
+		public GreetPage ()
+		{
+			InitializeComponent ();
+            sld1.Value = 0.01;
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            lblHello.Text = string.Format("Value is {0:F2}", e.NewValue*100);
+        }
+    }
+}
