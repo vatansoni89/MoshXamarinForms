f99a978185619293dc3aa464478c053f8cfa8177
Vatan Soni <vatansoni89@gmail.com>
2019-11-12 01:55:21 +0530

    GridLayout code behind (non-working)

--- HelloWorld/HelloWorld/GridPage.xaml.cs
+++ HelloWorld/HelloWorld/GridPage.xaml.cs
@@ -9,12 +9,39 @@ using Xamarin.Forms.Xaml;
 
 namespace HelloWorld
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class GridPage : ContentPage
-	{
-		public GridPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    /// <summary>
+    /// The commented code behind is not working if I comment the xaml code.
+    /// </summary>
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GridPage : ContentPage
+    {
+        public GridPage()
+        {
+            InitializeComponent();
+            //var grid = new Grid
+            //{
+            //    RowSpacing = 20,
+            //    ColumnSpacing = 40
+            //};
+            //var label = new Label { Text = "C# Label 1" };
+            //grid.Children.Add(label, 0, 0);
+            //Grid.SetRowSpan(label, 2);
+            //Grid.SetColumnSpan(label, 2);
+            //Grid.SetRow(label, 0);
+            //Grid.SetColumn(label, 0);
+
+            //grid.RowDefinitions.Add(new RowDefinition
+            //{
+            //    Height = new GridLength(100, GridUnitType.Absolute)
+            //});
+            //grid.RowDefinitions.Add(new RowDefinition
+            //{
+            //    Height = new GridLength(2, GridUnitType.Star)
+            //});
+            //grid.RowDefinitions.Add(new RowDefinition
+            //{
+            //    Height = new GridLength(1, GridUnitType.Auto)
+            //});
+        }
+    }
 }
