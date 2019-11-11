379965ca49c5216b5c4400af3c13042bcc1c7b60
Vatan Soni <vatansoni89@gmail.com>
2019-11-11 16:04:31 +0530

    StackLayout with code-behind example.

--- HelloWorld/HelloWorld/StackPage.xaml.cs
+++ HelloWorld/HelloWorld/StackPage.xaml.cs
@@ -9,12 +9,21 @@ using Xamarin.Forms.Xaml;
 
 namespace HelloWorld
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class StackPage : ContentPage
-	{
-		public StackPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StackPage : ContentPage
+    {
+        public StackPage()
+        {
+            InitializeComponent();
+
+            var layout = new StackLayout
+            {
+                Spacing = 40, /// all direction or try (,dcba)
+                Padding = new Thickness(0, 20, 0, 0),
+                Orientation = StackOrientation.Horizontal
+            };
+            layout.Children.Add(new Label { Text = "Label 1" });
+            Content = layout;
+        }
+    }
 }
