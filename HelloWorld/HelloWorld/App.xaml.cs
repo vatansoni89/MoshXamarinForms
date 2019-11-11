44658dc9e227a7362b363f744c282a3803d446c2
Vatan Soni <vatansoni89@gmail.com>
2019-11-11 15:56:23 +0530

    StackLayout basics with xaml example

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -12,7 +12,7 @@ namespace HelloWorld
         {
             InitializeComponent();
 
-            MainPage = new QuotesPage();
+            MainPage = new StackPage();
         }
 
         protected override void OnStart()
