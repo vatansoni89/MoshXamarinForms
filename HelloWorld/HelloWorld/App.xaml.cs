f025c1bf995ec2d8c1e789868081d42bf0bf6485
Vatan Soni <vatansoni89@gmail.com>
2019-11-13 19:32:23 +0530

    Adding Image with xaml and c# (xamarin forms version updated to 4)

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -14,7 +14,7 @@ namespace HelloWorld
 
             //MainPage = new StacklayoutExercize1();
             //MainPage = new Instagram();
-            MainPage = new AbsolutePage();
+            MainPage = new ImagePage();
         }
 
         protected override void OnStart()
---
+++ HelloWorld/HelloWorld/ImagePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ImagePage : ContentPage
+    {
+        public ImagePage()
+        {
+            InitializeComponent();
+            var imgSource = (UriImageSource)ImageSource.FromUri(new Uri("http://lorempixel.com/1920/1080/sports/7/"));
+
+            //below commented code also works.
+            //var imgSource = new UriImageSource { Uri = new Uri("http://lorempixel.com/1920/1080/sports/7/") };
+            imgSource.CachingEnabled = false; //by default its true.
+            imgSource.CacheValidity = TimeSpan.FromHours(1);
+
+            img1.Source = imgSource;
+
+            //img1.Source = "";
+        }
+    }
+}
