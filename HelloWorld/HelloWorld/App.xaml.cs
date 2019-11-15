aa826fc7c59d1926ecc9926e64e6fd21ee390b27
Vatan Soni <vatansoni89@gmail.com>
2019-11-15 14:06:04 +0530

    Basic list population example.

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -14,7 +14,7 @@ namespace HelloWorld
 
             //MainPage = new StacklayoutExercize1();
             //MainPage = new Instagram();
-            MainPage = new ImagesExcercise();
+            MainPage = new ListView();
         }
 
         protected override void OnStart()
