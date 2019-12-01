eef41f5ae2c01eada379f4ca3e85890d2ad945ca
Vatan Soni <vatansoni89@gmail.com>
2019-12-01 10:51:59 +0530

    Picker example with dynamic binding

--- HelloWorld/HelloWorld/PickerControlExample.xaml.cs
+++ HelloWorld/HelloWorld/PickerControlExample.xaml.cs
@@ -9,18 +9,44 @@ using Xamarin.Forms.Xaml;
 
 namespace HelloWorld
 {
+    public class ContactMethod
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class PickerControlExample : ContentPage
     {
+        private IList<ContactMethod> _contactMethods;
+
         public PickerControlExample()
         {
             InitializeComponent();
+            _contactMethods = GetContactMethods();
+            foreach (var method in GetContactMethods())
+            {
+                pkrContactMethod.Items.Add(method.Name);
+            }
         }
 
         private void Picker_SelectedIndexChanged(object sender, EventArgs e)
         {
-            var contactMethod = pkrContactMethod.Items[pkrContactMethod.SelectedIndex];
-            DisplayAlert("Selection", contactMethod, "OK");
+            var name = pkrContactMethod.Items[pkrContactMethod.SelectedIndex];
+
+            //If we wnat to get id then
+            var contactItem = _contactMethods.Single(x => x.Name == name);
+
+            DisplayAlert("Selection", name + " ID:" + contactItem.Id, "OK");
+        }
+
+        private IList<ContactMethod> GetContactMethods()
+        {
+            return new List<ContactMethod> {
+                new ContactMethod { Id=101, Name="SMS"},
+                new ContactMethod { Id=102, Name="Email"},
+                new ContactMethod { Id=103, Name="Pager"},
+            };
         }
     }
 }
